5a264d300a214a2fbf9c94a2bfe7e375f929e9f8
woozie164 <woozie663@gmail.com>
2014-05-14 15:13:49 +0200

    Removed the triangle from the corner

--- SteamPunk_Platform/HUD.cpp
+++ SteamPunk_Platform/HUD.cpp
@@ -3,73 +3,12 @@
 
 HUD::HUD(ID3D11Device* device, ID3D11DeviceContext * deviceContext, HWND hwnd, D3DXMATRIX proj, HINSTANCE hInstance, PlatformManager * platformManager)
 {
-	// Triangle
-	HRESULT result;
-	ID3D10Blob * VS_Buffer, *PS_Buffer;
-	result = D3DX11CompileFromFile(L"effect.fx", 0, 0, "VShader", "vs_5_0", 0, 0, 0, &VS_Buffer, 0, 0);
-	result = D3DX11CompileFromFile(L"effect.fx", 0, 0, "PShader", "ps_5_0", 0, 0, 0, &PS_Buffer, 0, 0);
-
-	result = device->CreateVertexShader(VS_Buffer->GetBufferPointer(), VS_Buffer->GetBufferSize(), NULL, &VS);
-	result = device->CreatePixelShader(PS_Buffer->GetBufferPointer(), PS_Buffer->GetBufferSize(), NULL, &PS);
-
-	deviceContext->VSSetShader(VS, 0, 0);
-	deviceContext->PSSetShader(PS, 0, 0);
-
-	float triangleData[] =
-	{
-		-0.875f, 1.0f, 0.0f,
-		-1.0f, 0.75f, 0.0f,
-		-0.75f, 0.75f, 0.0f
-	};
-
-	D3D11_SUBRESOURCE_DATA vertexBufferData;
-	ZeroMemory(&vertexBufferData, sizeof(vertexBufferData));
-	vertexBufferData.pSysMem = triangleData;
-
-	D3D11_BUFFER_DESC bufferDesc;
-	bufferDesc.ByteWidth = sizeof(float)* 3 * 3;
-	bufferDesc.Usage = D3D11_USAGE_DEFAULT;
-	bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-	bufferDesc.CPUAccessFlags = 0;
-	bufferDesc.MiscFlags = 0;
-	bufferDesc.StructureByteStride = 0;
-
-	result = device->CreateBuffer(&bufferDesc, &vertexBufferData, &triangleVertBuffer);
-	UINT stride = sizeof(float)* 3;
-	UINT offset = 0;
-	deviceContext->IASetVertexBuffers(0, 1, &triangleVertBuffer, &stride, &offset);
-
-	D3D11_INPUT_ELEMENT_DESC layout;
-	layout.SemanticName = "POSITION";
-	layout.SemanticIndex = 0;
-	layout.Format = DXGI_FORMAT_R32G32B32_FLOAT;
-	layout.InputSlot = 0;
-	layout.AlignedByteOffset = 0;
-	layout.InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
-	layout.InstanceDataStepRate = 0;
-
-	result = device->CreateInputLayout(&layout, 1, VS_Buffer->GetBufferPointer(), VS_Buffer->GetBufferSize(), &vertLayout);
-
-	deviceContext->IASetInputLayout(vertLayout);
-	deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-
-	D3D11_VIEWPORT viewport;
-	ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
-
-	viewport.MinDepth = 0.0f;
-	viewport.MaxDepth = 1.0f;
-	viewport.TopLeftX = 0;
-	viewport.TopLeftY = 0;
-	viewport.Width = 800;
-	viewport.Height = 640;
-
-	deviceContext->RSSetViewports(1, &viewport);
-
 	this->device = device;
 	this->deviceContext = deviceContext;
 
 	// Render a texture
 	ID3D10Blob * textureVSBuffer, *texturePSBuffer;
+	HRESULT result;
 	result = D3DX11CompileFromFile(L"texture.fx", 0, 0, "VShader", "vs_5_0", 0, 0, 0, &textureVSBuffer, NULL, 0);
 	result = D3DX11CompileFromFile(L"texture.fx", 0, 0, "PShader", "ps_5_0", 0, 0, 0, &texturePSBuffer, NULL, 0);
 
@@ -81,12 +20,12 @@ HUD::HUD(ID3D11Device* device, ID3D11DeviceContext * deviceContext, HWND hwnd, D
 
 	std::vector<float> cogData;
 	// Each vertex requires 3 floats for position, and 2 floats for texcoordinates.
-	// Each cog consists of 4 vertices.
+	// Each cog consists of 6 vertices.
 	for (int i = 0; i < platformManager->gearsTotal; i++)
 	{
 		// Each cog texture is drawn next to each other vertically.
 		// The offset is 0.125 in screen cordinates.
-		float offset = i * 0.125;
+		float offset = i * 0.125f;
 		cogData.push_back(-1.0f + offset); cogData.push_back(0.875f); cogData.push_back(0.0f);
 		cogData.push_back(0.0f); cogData.push_back(1.0f);
 		cogData.push_back(-1.0f + offset); cogData.push_back(1.0f); cogData.push_back(0.0f);
@@ -96,13 +35,18 @@ HUD::HUD(ID3D11Device* device, ID3D11DeviceContext * deviceContext, HWND hwnd, D
 
 		cogData.push_back(-0.875f + offset); cogData.push_back(1.0f); cogData.push_back(0.0f);
 		cogData.push_back(1.0f); cogData.push_back(0.0f);
-
+		cogData.push_back(-1.0f + offset); cogData.push_back(1.0f); cogData.push_back(0.0f);
+		cogData.push_back(0.0f); cogData.push_back(0.0f);
+		cogData.push_back(-0.875f + offset); cogData.push_back(0.875f); cogData.push_back(0.0f);
+		cogData.push_back(1.0f); cogData.push_back(1.0f);
 	}
 
+	D3D11_SUBRESOURCE_DATA vertexBufferData;
 	ZeroMemory(&vertexBufferData, sizeof(vertexBufferData));
 	vertexBufferData.pSysMem = cogData.data();
 
-	bufferDesc.ByteWidth = sizeof(float)* (3 + 2) * platformManager->gearsTotal * 4;
+	D3D11_BUFFER_DESC bufferDesc;
+	bufferDesc.ByteWidth = sizeof(float) * cogData.size();
 	bufferDesc.Usage = D3D11_USAGE_DEFAULT;
 	bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
 	bufferDesc.CPUAccessFlags = 0;
@@ -110,8 +54,8 @@ HUD::HUD(ID3D11Device* device, ID3D11DeviceContext * deviceContext, HWND hwnd, D
 	bufferDesc.StructureByteStride = 0;
 
 	result = device->CreateBuffer(&bufferDesc, &vertexBufferData, &quadVertBuffer);
-	stride = sizeof(float)* (3 + 2);
-	offset = 0;
+	UINT stride = sizeof(float) * (3 + 2);
+	UINT offset = 0;
 	deviceContext->IASetVertexBuffers(0, 1, &quadVertBuffer, &stride, &offset);
 
 	D3D11_INPUT_ELEMENT_DESC texturefxLayout[2];
@@ -133,7 +77,7 @@ HUD::HUD(ID3D11Device* device, ID3D11DeviceContext * deviceContext, HWND hwnd, D
 
 	result = device->CreateInputLayout(texturefxLayout, 2, textureVSBuffer->GetBufferPointer(), textureVSBuffer->GetBufferSize(), &quadLayout);
 	deviceContext->IASetInputLayout(quadLayout);
-	deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+	deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
 	result = D3DX11CreateShaderResourceViewFromFile(device, L"Resources/Textures/cog_full.png", NULL, NULL, &cog_fullTexture, NULL);
 	result = D3DX11CreateShaderResourceViewFromFile(device, L"Resources/Textures/cog_empty.png", NULL, NULL, &cog_emptyTexture, NULL);
@@ -184,16 +128,6 @@ void HUD::Update()
 
 void HUD::Draw()
 {
-	// Triangle
-	deviceContext->VSSetShader(VS, 0, 0);
-	deviceContext->PSSetShader(PS, 0, 0);
-	UINT stride = sizeof(float)* 3;
-	UINT offset = 0;
-	deviceContext->IASetInputLayout(vertLayout);
-	deviceContext->IASetVertexBuffers(0, 1, &triangleVertBuffer, &stride, &offset);
-	deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-	deviceContext->Draw(3, 0);
-
 	// Cog cog_fullTexture
 	float blendFactor[] = { 0.75f, 0.75f, 0.75f, 1.0f };
 	deviceContext->OMSetBlendState(blendState, blendFactor, 0xffffffff);
@@ -201,23 +135,19 @@ void HUD::Draw()
 	deviceContext->PSSetShader(texturePS, 0, 0);
 	deviceContext->PSSetShaderResources(0, 1, &cog_fullTexture);
 	deviceContext->PSSetSamplers(0, 1, &samplerState);
-	stride = sizeof(float)* (3 + 2);
-	offset = 0;
+	UINT stride = sizeof(float) * (3 + 2);
+	UINT offset = 0;
 	deviceContext->IASetInputLayout(quadLayout);
 	deviceContext->IASetVertexBuffers(0, 1, &quadVertBuffer, &stride, &offset);
-	deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+	deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
-	int i;
-	for (i = 0; i < platformManager->gearsFound; i++)
-	{
-		deviceContext->Draw(4, 4 * i);
-	}
-	deviceContext->PSSetShaderResources(0, 1, &cog_emptyTexture);
-	for (; i < platformManager->gearsTotal; i++)
-	{
-		deviceContext->Draw(4, 4 * i);
-	}
-	
+	int gearsFound = platformManager->gearsFound;
+	int gearsMissing = platformManager->gearsTotal - gearsFound;
+
+	deviceContext->Draw(6 * gearsFound, 0);
+	deviceContext->PSSetShaderResources(0, 1, &cog_emptyTexture);	
+	deviceContext->Draw(6 * gearsMissing, 6 * gearsFound);
+
 	// Reset blend state
 	deviceContext->OMSetBlendState(0, 0, 0xffffffff);
 }
--- SteamPunk_Platform/HUD.h
+++ SteamPunk_Platform/HUD.h
@@ -9,18 +9,14 @@
 
 class HUD : public Screen
 {
-	Button startButton;
 	ID3D11Device * device;
 	ID3D11DeviceContext * deviceContext;
-	ID3D11Buffer* triangleVertBuffer;
 	ID3D11Buffer* quadVertBuffer;
-	ID3D11VertexShader * VS;
-	ID3D11PixelShader * PS;
 	ID3D11VertexShader * textureVS;
 	ID3D11PixelShader * texturePS;
 	ID3D11ShaderResourceView * cog_fullTexture, * cog_emptyTexture;
 	ID3D11SamplerState * samplerState;
-	ID3D11InputLayout* vertLayout, * quadLayout;
+	ID3D11InputLayout * quadLayout;
 	ID3D11BlendState * blendState;
 	PlatformManager * platformManager;
 public:
